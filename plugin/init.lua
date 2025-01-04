b69d597b9bb6f0216492234fed10d466688afb36
elbunuelo <mail@nicolasarias.co>
2025-01-04 18:25:44 -0500

    Make keymap consistent with what I have in zk

--- plugin/init.lua
+++ plugin/init.lua
@@ -17,4 +17,4 @@ end, {})
 
 vim.api.nvim_set_keymap('n', '<leader>Nd', ':FloatDaily<CR>', { desc = 'Open daily note in floating window.' })
 vim.api.nvim_set_keymap('n', '<leader>Nw', ':FloatWeekly<CR>', { desc = 'Open weekly note in floating window.' })
-vim.api.nvim_set_keymap('n', '<leader>Np', ':FloatPick<CR>', { desc = 'Pick a note and open it in a floating window.' })
+vim.api.nvim_set_keymap('n', '<leader>No', ':FloatPick<CR>', { desc = 'Pick a note and open it in a floating window.' })
