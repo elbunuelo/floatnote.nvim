8a3a5a7e1540674c8af9425a7d2ef765dfa6f481
Nico Arias-Gonzalez <nariasgonzalez@aha.io>
2025-03-04 10:42:52 -0500

    Remove daily notes options

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,11 +1,6 @@
 local GROUP = {
-  daily = "daily",
   weekly = "weekly",
 }
-
-vim.api.nvim_create_user_command("FloatDaily", function()
-  require("floatnote").open_note(GROUP.daily)
-end, {})
 
 vim.api.nvim_create_user_command("FloatWeekly", function()
   require("floatnote").open_note(GROUP.weekly)
@@ -23,7 +18,6 @@ vim.api.nvim_create_user_command("FloatLast", function()
   require("floatnote").last_note()
 end, {})
 
-vim.api.nvim_set_keymap("n", "<leader>Nt", ":FloatDaily<CR>", { desc = "Open daily note in floating window." })
 vim.api.nvim_set_keymap("n", "<leader>Nw", ":FloatWeekly<CR>", { desc = "Open weekly note in floating window." })
 vim.api.nvim_set_keymap("n", "<leader>No", ":FloatPick<CR>", { desc = "Pick a note and open it in a floating window." })
 vim.api.nvim_set_keymap("n", "<leader>Nn", ":FloatNew<CR>", { desc = "Create a new note." })
